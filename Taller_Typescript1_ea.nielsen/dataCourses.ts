98bc13fcfab943a0c6a265318de4165d9a6e7497
Erik <ea.nielsen@uniandes.edu.co>
2020-10-21 18:29:08 -0500

    taller 1

---
+++ Taller_Typescript1_ea.nielsen/dataCourses.ts
@@ -0,0 +1,11 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Rubby Casallas", 3),
+  new Course("TI", "Elizabeth Muñoz", 3),
+  new Course("Sistrans", "Claudia JImenez", 3),
+  new Course("Sisconger", "Sebastian Sanchez", 3),
+  new Course("Funprod", "Carlos Montoya", 3)
+ 
+]
---
+++ Taller_Typescript1_ea.nielsen/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Rubby Casallas", 3),
+    new Course("TI en las organizaciones", "Elizabeth Muñoz", 3),
+    new Course("Sistrans", "Claudia Jimenez", 3),
+    new Course("Sisconger", "Sebastian Sanchez", 3),
+    new Course("Funprod", "Carlos Montoya", 3)
+];
